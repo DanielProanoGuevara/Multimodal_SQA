624cb2379b9cd7cf72ce16b9e659682d529ddca8
Daniel Proaño <daniel.p.g.ecu@gmail.com>
2024-07-16 17:49:27 +0100

    added wavelet denoising

--- Throwaway.py
+++ Throwaway.py
@@ -5,7 +5,7 @@ Created on Fri Jul 12 11:16:31 2024
 @author: danie
 """
 
-#%% 
+# %%
 # Imports
 
 import os
@@ -13,15 +13,16 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+import pywt
 
 
-#%% 
+# %%
 # Throwaway Functions
 
 def load_wav_files(directory):
     # List to store the data from all .wav files
     data_list = []
-    
+
     # Loop through all files in the given directory
     for filename in os.listdir(directory):
         # Check if the file is a .wav file
@@ -32,54 +33,85 @@ def load_wav_files(directory):
             samplerate, data = wavfile.read(file_path)
             # Append the data to the list
             data_list.append(data)
-    
+
     # Combine all data into a single numpy array
     combined_data = np.concatenate(data_list)
-    
+
     return combined_data
+
 
 def plot_histogram_with_gaussian(data):
     # Calculate histogram
-    counts, bins, _ = plt.hist(data, bins=50, density=True, alpha=0.6, color='g')
-    
+    counts, bins, _ = plt.hist(
+        data, bins=50, density=True, alpha=0.6, color='g')
+
     # Fit a Gaussian distribution to the data
     mu, std = norm.fit(data)
-    
+
     # Plot the Gaussian fit
     xmin, xmax = plt.xlim()
     x = np.linspace(xmin, xmax, 100)
     p = norm.pdf(x, mu, std)
     plt.plot(x, p, 'k', linewidth=2)
-    
+
     title = f"Fit results: mu = {mu:.2f},  std = {std:.2f}"
     plt.title(title)
-    
+
     plt.show()
-    
+
     return mu, std
 
-#%%
+
+def ValSUREThresh(X):
+    """
+    Adaptive Threshold Selection Using Principle of SURE.
+
+    Parameters
+    ----------
+    X: array
+         Noisy Data with Std. Deviation = 1
+
+    Returns
+    -------
+    tresh: float
+         Value of Threshold
+
+    """
+    n = np.size(X)
+
+    # a = mtlb_sort(abs(X)).^2
+    a = np.sort(np.abs(X))**2
+
+    c = np.linspace(n-1, 0, n)
+    s = np.cumsum(a)+c*a
+    risk = (n - (2 * np.arange(n)) + s)/n
+    # risk = (n-(2*(1:n))+(cumsum(a,'m')+c(:).*a))/n;
+    ibest = np.argmin(risk)
+    THR = np.sqrt(a[ibest])
+    return THR
+
+
+# %%
 # Import and analyze the dataset
 
 # Directory containing the files
-directory = '../Physionet_2016_training/training-f'
+directory = '../Physionet_2016_training/training-a/a0001.wav'
 
 # Load and combine .wav files
-combined_wav_data = load_wav_files(directory)
+samplerate, data = wavfile.read(directory)
 
 # Print the shape of the combined array
-print(combined_wav_data.shape)
+print(data.shape)
 
-#%%
-# Generate histogram and fit Gaussian
-mean, std_dev = plot_histogram_with_gaussian(combined_wav_data)
+# %%
+# Denoise
 
-# Calculate additional statistics
-data_min = np.min(combined_wav_data)
-data_max = np.max(combined_wav_data)
+wavelet = 'coif4'
 
-# Print the statistics
-print(f"Mean: {mean}")
-print(f"Standard Deviation: {std_dev}")
-print(f"Minimum: {data_min}")
-print(f"Maximum: {data_max}")
+
+# Plot the original, noisy, and denoised signals
+plt.figure(figsize=(10, 6))
+plt.plot(data, label='Noisy Signal')
+plt.plot(denoised_signal, label='Denoised Signal')
+plt.legend()
+plt.show()
--- preprocessing_lib.py
+++ preprocessing_lib.py
@@ -25,7 +25,34 @@ signal, enforcing that the Shannon-Nyquist criteria is always met by using
 polyphase antialias filters. It also manages the interpolation - decimation
 opperations needed for meeting the target frequency.
 
--
+- butterworth_filter(data, filter_topology, order, fs, fc): filters the input
+signal. The topology must be specified, as well as the order, sampling
+frequency, and cutoff frequencies. The signal is forward-backward filtered,
+removing the phase shifting, and doubling the order of the filters. It also
+uses second order sections to ensure the stability of the filter.
+
+- schmidt_spike_removal(original_signal, fs): This function removes the spikes
+in a signal as done by Schmidt et al in the paper:
+Schmidt, S. E., Holst-Hansen, C., Graff, C., Toft, E., & Struijk, J. J. (2010).
+Segmentation of heart sound recordings by a duration-dependent
+hidden Markov model.
+Physiological Measurement, 31(4), 513-29.
+This function was translated by ChatGPT.
+
+- wavelet_denoise(data, decomposition_level, wavelet_family, risk_estimator,
+                  shutdown_bands): This function filters and
+denoises the inpud data based on wavelet decomposition. It requires a risk
+estimation function to opperate. This function assumes that the input data is
+standardized, thus, the noise standard deviation = 1
+
+- val_SURE_threshold(X): computes the threshold for the wavelet denoise
+function based on SURE. Function taken from
+https://pyyawt.readthedocs.io/_modules/pyyawt/denoising.html#thselect
+
+- universal_threshold(coefficients): computes the threshold for wavelet
+denoise, this function allows for the implementation of VisuShrink denoising
+approach
+https://academic.oup.com/biomet/article-abstract/81/3/425/256924?redirectedFrom=fulltext
 
 
 @author: Daniel Proaño-Guevara.
@@ -35,9 +62,6 @@ opperations needed for meeting the target frequency.
 @version: 0.1
 """
 
-# TODO: Butterworth Filter.
-# TODO: Schmidt interference removal.
-# TODO: Wavelet denoising. Select bands to remove, select band to infer noise.
 # TODO: Homomorphic Envelogram.
 # TODO: Hilbert Envelope.
 # TODO: Wavelet Envelope.
@@ -45,6 +69,8 @@ opperations needed for meeting the target frequency.
 
 import numpy as np
 from scipy.signal import firwin, resample_poly
+from scipy import signal
+import pywt
 
 
 def resolution_normalization(data, resolution, centered_on_zero=True):
@@ -148,3 +174,195 @@ def downsample(input_signal, orig_freq, target_freq):
         input_signal, up, down, window=fir_filter)
 
     return downsampled_signal
+
+
+def butterworth_filter(data, filter_topology, order, fs, fc):
+    """
+    Filter the input data with a forward-backward structure.
+
+    Parameters
+    ----------
+    data : TYPE: numpy.ndarray
+        DESCRIPTION: Is the signal to be filtered.
+    filter_topology : TYPE: String
+        DESCRIPTION: The topology of the filter {'lowpass', 'highpass',
+                                                 'bandpass', 'bandstop'}.
+    order : TYPE: int
+        DESCRIPTION: The order of the filter.
+    fs : TYPE: int
+        DESCRIPTION: The sampling frequency in Hz.
+    fc : TYPE: Int, [Int, Int]
+        DESCRIPTION: The cutoff frequency, or frequencies. For bandpass or
+        bandstop, pass the coefficients between brackets [a, b].
+
+    Returns
+    -------
+    TYPE: numpy.ndarray
+        DESCRIPTION: The filtered signal.
+
+    """
+    coefficients = signal.butter(order, fc, btype=filter_topology,
+                                 output="sos", fs=fs)
+    return signal.sosfiltfilt(coefficients, data)
+
+
+def schmidt_spike_removal(original_signal, fs):
+    """
+    Remove the spikes in a signal.
+
+    Parameters:
+    original_signal (numpy array): The original (1D) audio signal array
+    fs (int): The sampling frequency (Hz)
+
+    Returns:
+    numpy array: The audio signal with any spikes removed.
+    """
+    # Find the window size (500 ms)
+    windowsize = round(fs / 2)
+
+    # Find any samples outside of an integer number of windows
+    trailingsamples = len(original_signal) % windowsize
+
+    # Reshape the signal into a number of windows
+    sampleframes = np.reshape(original_signal[:-trailingsamples],
+                              (windowsize, -1), order='F')
+
+    # Find the MAAs
+    MAAs = np.max(np.abs(sampleframes), axis=0)
+
+    # While there are still samples greater than 3 * the median value of the
+    # MAAs, then remove those spikes
+    while np.any(MAAs > np.median(MAAs) * 3):
+        # Find the window with the max MAA
+        window_num = np.argmax(MAAs)
+
+        # Find the position of the spike within that window
+        spike_position = np.argmax(np.abs(sampleframes[:, window_num]))
+
+        # Finding zero crossings (where there may not be actual 0 values, just
+        # a change from positive to negative)
+        zero_crossings = np.abs(np.diff(np.sign(
+            sampleframes[:, window_num]))) > 1
+
+        # Find the start of the spike, finding the last zero crossing before
+        # spike position. If that is empty, take the start of the window
+        if any(zero_crossings[:spike_position]):
+            spike_start = np.max(np.nonzero(
+                zero_crossings[:spike_position])) + 1
+        else:
+            spike_start = 0
+
+        # Find the end of the spike, finding the first zero crossing after
+        # spike position. If that is empty, take the end of the window
+        if any(zero_crossings[spike_position:]):
+            spike_end = np.min(np.nonzero(zero_crossings[spike_position:]))
+            + spike_position
+        else:
+            spike_end = windowsize - 1
+
+        # Set to zero
+        sampleframes[spike_start:spike_end+1, window_num] = 0.0001
+
+        # Recalculate MAAs
+        MAAs = np.max(np.abs(sampleframes), axis=0)
+
+    # Reshape the despiked signal back to 1D
+    despiked_signal = np.reshape(sampleframes, -1, order='F')
+
+    # Add the trailing samples back to the signal
+    despiked_signal = np.concatenate((despiked_signal,
+                                      original_signal[len(despiked_signal):]))
+
+    return despiked_signal
+
+
+def wavelet_denoise(data, decomposition_level, wavelet_family,
+                    risk_estimator, shutdown_bands):
+    """
+    Wavelet denoising and filtering.
+
+    Parameters
+    ----------
+    data : TYPE: numpy.ndarray
+        DESCRIPTION: The data to be processed.
+    decomposition_level : TYPE: int
+        DESCRIPTION: The level of wavelet decompositions.
+    wavelet_family : TYPE: string
+        DESCRIPTION: The wavelet family available in the pywt library.
+    risk_estimator : TYPE: function
+        DESCRIPTION: function that estimates the risk and sets the elimination
+        threshold.
+    shutdown_bands : TYPE: array
+        DESCRIPTION: array of bands to be shut down.
+
+    Returns
+    -------
+    TYPE: numpy.ndarray
+        DESCRIPTION: The filtered and denoised data.
+
+    """
+    # Wavelet decomposition
+    wv_coeff = pywt.wavedec(data, wavelet=wavelet_family,
+                            level=decomposition_level)
+    # Band denoising
+    for band_index in range(len(wv_coeff)-1):
+        # Compute lambda threshold for risk reduction
+        lambda_threshold = risk_estimator(wv_coeff[band_index + 1])
+        # Threshold the band
+        wv_coeff[band_index + 1] = pywt.threshold(data, value=lambda_threshold,
+                                                  mode='garrote')
+    # Remove mean in approximation band
+    wv_coeff[0] -= np.mean(wv_coeff[0])
+    # Shutdown bands
+    for band in shutdown_bands:
+        wv_coeff[band][:] = 0
+    # Reconstruct the signal
+    return pywt.waverec(wv_coeff, wavelet=wavelet_family)
+
+
+def val_SURE_threshold(X):
+    """
+    Adaptive Threshold Selection Using Principle of SURE.
+
+    Parameters
+    ----------
+    X: array
+         Noisy Data with Std. Deviation = 1
+
+    Returns
+    -------
+    tresh: float
+         Value of Threshold
+
+    """
+    n = np.size(X)
+
+    # a = mtlb_sort(abs(X)).^2
+    a = np.sort(np.abs(X))**2
+
+    c = np.linspace(n-1, 0, n)
+    s = np.cumsum(a)+c*a
+    risk = (n - (2 * np.arange(n)) + s)/n
+    # risk = (n-(2*(1:n))+(cumsum(a,'m')+c(:).*a))/n;
+    ibest = np.argmin(risk)
+    THR = np.sqrt(a[ibest])
+    return THR
+
+
+def universal_threshold(coefficients):
+    """
+    Calculate the universal threshold for risk reduction.
+
+    Parameters
+    ----------
+    coefficients : TYPE: numpy.ndarray
+        DESCRIPTION: The coefficients to estimate the risk from.
+
+    Returns
+    -------
+    TYPE: float
+        DESCRIPTION: The value of threshold.
+
+    """
+    n = len(coefficients)
+    return np.sqrt(2 * np.log(n))
