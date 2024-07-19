b17d65113685d32c554753b4726d6c2000fd2bd4
Daniel Proaño <daniel.p.g.ecu@gmail.com>
2024-07-19 16:20:25 +0100

    creation of feature extraction lib

---
+++ feature_extraction_lib.py
@@ -0,0 +1,30 @@
+"""
+ECG and PCG feature extraction library.
+
+@file: feature_extraction_lib.py
+
+@coding: utf_8
+
+# TODO
+@description: This module intends to contain all the preprocessing functions
+necessary for the digital signal processing of several databases, considering
+the ECG and PCG signals. The databases that this code is meant for are:
+
+PCG:
+    - Physionet 2016 a-f
+
+@functions:
+-
+
+
+@author: Daniel Proaño-Guevara.
+
+@creationDate: 2024-07-19
+
+@version: 0.1
+"""
+
+# TODO: Homomorphic Envelogram.
+# TODO: Hilbert Envelope.
+# TODO: Wavelet Envelope.
+# TODO: Power Spectral Density Envelope.
